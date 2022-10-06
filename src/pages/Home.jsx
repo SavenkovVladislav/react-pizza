d1aa92301fea88b8a54fb03ebc6bf97d02e6c5b5
Vlad <savenkov.vla@yandex.ru>
2022-10-06 15:46:08 +0700

    add Pagination.jsx, filter changed

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,13 +4,15 @@ import Categories from '../components/Categories/Categories'
 import Sort from '../components/Sort/Sort'
 import PizzaBlock from '../components/PizzaBlock/PizzaBlock'
 import Skeleton from '../components/PizzaBlock/Skeleton'
+import Pagination from '../components/Pagination/Pagination'
+
 import { useState, useEffect } from 'react'
 
 const Home = ({ searchValue }) => {
 	const [items, setItems] = useState([])
 	const [isLoading, setIsLoading] = useState(true)
-
 	const [categoryId, setCategoryId] = useState(0)
+	const [currentPage, setCurrentPage] = useState(1)
 	const [sortType, setSortType] = useState({
 		name: 'популярности',
 		sortProperty: 'rating',
@@ -24,10 +26,11 @@ const Home = ({ searchValue }) => {
 		const order = sortType.sortProperty.includes('-') ? 'asc' : 'desc'
 		const sortBy = sortType.sortProperty.replace('-', '')
 		const category = categoryId > 0 ? `category=${categoryId}` : ''
+		const search = searchValue ? `&search=${searchValue}` : ''
 
 		// когда произойдет запрос
 		fetch(
-			`https://632a9f84713d41bc8e735d54.mockapi.io/items?${category}&sortBy=${sortBy}&order=${order}`
+			`https://632a9f84713d41bc8e735d54.mockapi.io/items?page=${currentPage}&limit=4&${category}&sortBy=${sortBy}&order=${order}${search}`
 		)
 			//тогда (then) верни ответ в формате .json
 			.then(res => {
@@ -39,20 +42,9 @@ const Home = ({ searchValue }) => {
 				setIsLoading(false)
 			})
 		window.scrollTo(0, 0)
-	}, [categoryId, sortType])
+	}, [categoryId, sortType, searchValue, currentPage])
 
-	// в переменную pizzas записывается массив items, который тянется с бэка.
-	// 1) фильтруется массив items с помощью метода filter(). Каждый объект массива items с ключем title переводится в нижний регистр. После, с помощью метода includes() проверяется есть ли в этом title значение searchValue, которое тоже переводится в нижний регистр. Таким образом массив items фильтруется и преобразуется в массив в котором есть только те объекты, в которых ключ title соответствует значению searchValue, которое перезаписывается в компоненте Search.tsx
-	// 2) получившийся массив items мапится
-	// все это нужно для того, чтобы пиццы фитровались в зависимости от того, что юзер пишет в инпуте. Этот вариант подходит только для статичных массивов
-	const pizzas = items
-		.filter(obj => {
-			if (obj.title.toLowerCase().includes(searchValue.toLowerCase())) {
-				return true
-			}
-			return false
-		})
-		.map(obj => <PizzaBlock key={obj.id} {...obj} />)
+	const pizzas = items.map(obj => <PizzaBlock key={obj.id} {...obj} />)
 
 	const skeletons = [...new Array(6)].map((_, index) => (
 		<Skeleton key={index} />
@@ -69,6 +61,7 @@ const Home = ({ searchValue }) => {
 			</div>
 			<h2 class='content__title'>Все пиццы</h2>
 			<div class='content__items'>{isLoading ? skeletons : pizzas}</div>
+			<Pagination onChangePage={number => setCurrentPage(number)} />
 		</>
 	)
 }
